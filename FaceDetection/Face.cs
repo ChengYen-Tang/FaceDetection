b1480b3111953c052613873d30ce0ece1ded11b4
Kenneth Tang <kenneth@nssh.onmicrosoft.com>
2020-03-17 00:57:14 +0800

    加入專案檔案。

---
+++ FaceDetection/Face.cs
@@ -0,0 +1,17 @@
+﻿using Emgu.CV.Dnn;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FaceDetection
+{
+    public abstract class Face
+    {
+        protected Net model;
+
+        protected Face()
+        {
+
+        }
+    }
+}
