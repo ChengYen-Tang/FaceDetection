b5f2d955e2c2018040bff379fab0ac7ead382ca2
Kenneth Tang <kenneth@nssh.onmicrosoft.com>
2020-03-18 02:59:49 +0800

    臉部偵測

---
+++ FaceDetectionWinForms/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace FaceDetectionWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 800);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(335, 123);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Camera";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(613, 800);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(335, 123);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Stop Camera";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(132, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(809, 761);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2226, 1099);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private Emgu.CV.UI.ImageBox imageBox1;
+    }
+}
+
---
+++ FaceDetectionWinForms/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using FaceDetection;
+
+namespace FaceDetectionWinForms
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture capture = null;
+        private readonly FaceAnalysis faceDetection = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            faceDetection = new FaceAnalysis();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            capture = new VideoCapture();
+            capture.ImageGrabbed += ProcessFrame;
+            capture.Start();
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            if (capture != null)
+            {
+                using (Mat frame = new Mat())
+                {
+                    capture.Retrieve(frame, 0);
+                    //IEnumerable<int[]> faces = faceDetection.Detect(frame);
+                    //IEnumerable<Rectangle> facesRectangle = faces.Select(face => new Rectangle(face[0], face[1], face[2], face[3]));
+                    //foreach (Rectangle face in facesRectangle)
+                    //    CvInvoke.Rectangle(frame, face, new Bgr(Color.Green).MCvScalar, 2);
+                    imageBox1.Image = frame.ToImage<Bgr, byte>();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            capture.Stop();
+            capture.Dispose();
+        }
+    }
+}
